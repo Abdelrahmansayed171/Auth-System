737150f9b32f1212a96e0207f7cf3ef0e21dafd4
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-29 08:56:04 +0200

    adding some shadows

--- authentication_system/tailwind.config.js
+++ authentication_system/tailwind.config.js
@@ -10,8 +10,11 @@ export default {
       borderRadius: {
         '4xl': '4rem', // Slightly larger than 3xl (1.875rem)
       },
+      boxShadow:{
+        'custom-light': '10px 0 20px -5px rgba(56, 189, 248, 0.8)', // for bg-blue-400
+        'custom-dark': '10px 0 20px -5px rgba(26, 35, 126, 0.8)',  // for dark:bg-indigo-700
+      },
     },
   },
   plugins: [],
 }
-
