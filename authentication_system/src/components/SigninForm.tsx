e15a1327919e1892e83ff1d1edfdde3319d91622
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-29 03:23:03 +0200

    adding ThemeToggler in the form, but no color changes happen

--- authentication_system/src/components/SigninForm.tsx
+++ authentication_system/src/components/SigninForm.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { LockKeyhole } from 'lucide-react';
+import ThemeToggler from './ThemeToggler';
 
 interface AuthLayoutArgs{
     children: React.ReactNode;
@@ -10,7 +11,8 @@ interface AuthLayoutArgs{
 
 const SigninForm = ({children, title, subtitle}: AuthLayoutArgs) => {
   return (
-    <div className='min-h-screen flex flex-col lg:flex-row bg-gray-50 dark:bg-gray-900 transition-colors'>
+      <div className='min-h-screen flex flex-col lg:flex-row bg-gray-50 dark:bg-gray-900 transition-colors'>
+        <ThemeToggler/>
         <div className='hidden lg:flex lg:w-1/2 bg-blue-400 dark:bg-indigo-700 relative overflow-hidden rounded-r-4xl'>
             <img 
                 src='https://images.unsplash.com/photo-1496167117681-944f702be1f4?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
